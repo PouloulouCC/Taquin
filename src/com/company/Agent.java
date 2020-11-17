ad84650408b837053d00293140e2465370761fc0
RebekaBy <rebeka.by@hotmail.com>
2020-11-17 15:11:15 +0100

    Constructeur Agent

--- src/com/company/Agent.java
+++ src/com/company/Agent.java
@@ -1,13 +1,20 @@
 package com.company;
 
+import java.util.List;
+
 public class Agent {
     private Environnement e;
     private int posX, posY;
     private boolean directionLibre;
+    private int posXFinal, posYFinal;
+    private List<Message> listeMessage;
 
-    public Agent(Environnement e, int posX, int posY) {
+
+    public Agent(Environnement e, int posX, int posY, int posXFinal, int posYFinal) {
         this.e = e;
         this.posX = posX;
         this.posY = posY;
+        this.posXFinal = posXFinal;
+        this.posYFinal = posYFinal;
     }
 }
