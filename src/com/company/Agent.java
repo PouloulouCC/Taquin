80926077db41106a3d8e6b0b3f9813987dc62919
RebekaBy <rebeka.by@hotmail.com>
2020-11-17 14:45:59 +0100

    Ajout classe Agent et Environnement

--- src/com/company/Agent.java
+++ src/com/company/Agent.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Agent {
+    private Environnement e;
+    private int posX, posY;
+    private boolean directionLibre;
+
+    public Agent(Environnement e, int posX, int posY) {
+        this.e = e;
+        this.posX = posX;
+        this.posY = posY;
+    }
 }
---
+++ src/com/company/Environnement.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.List;
+
+public class Environnement {
+    private String[][] plateau;
+    private int n;
+    private int nbAgents;
+    private List<Agent> listeAgents;
+
+    public Environnement(int n, int nbAgents){
+        this.n = n;
+        this.nbAgents = nbAgents;
+    }
+}
